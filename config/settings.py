ad9f2accee5c84b2d537bceba62643e08eb239b8
thdgkdud75 <dnwlsfirst75@gmail.com>
2026-03-17 20:21:23 +0900

    render 배포 위해서 수정

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,12 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://cartel-lab.onrender.com",
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
